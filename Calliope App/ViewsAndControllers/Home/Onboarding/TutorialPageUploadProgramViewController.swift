16c4043486d19047d47a447b27e0be820a073c84
Tassilo Karge <tassilo.karge@web.de>
2019-11-15 00:35:42 +0100

    continued implementation of onboarding vc

---
+++ Calliope App/ViewsAndControllers/Home/Onboarding/TutorialPageUploadProgramViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TutorialPageUploadProgramViewController.swift
+//  Calliope App
+//
+//  Created by Tassilo Karge on 14.11.19.
+//  Copyright © 2019 calliope. All rights reserved.
+//
+
+import UIKit
+
+class TutorialPageUploadProgramViewController: TutorialPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
