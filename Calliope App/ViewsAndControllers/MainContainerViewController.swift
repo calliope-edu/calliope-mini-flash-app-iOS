670c847af344b1e9ec5c13554a1aa82fc26e5d31
Tassilo Karge <tassilo.karge@web.de>
2021-04-08 14:11:34 +0200

    proper viewcontroller hierarchy for connection button

--- Calliope App/ViewsAndControllers/MainContainerViewController.swift
+++ Calliope App/ViewsAndControllers/MainContainerViewController.swift
@@ -17,22 +17,33 @@ class MainContainerViewController: UITabBarController, UITabBarControllerDelegat
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 
-		let window = (UIApplication.shared.delegate!.window!)!
+        DispatchQueue.main.async {
+            //called on the non-concurrent main queue so no further synchronization necessary
+            self.addConnectionViewController()
+        }
+	}
 
-		let connectionVC = UIStoryboard(name: "ConnectionView", bundle: nil).instantiateInitialViewController() as! MatrixConnectionViewController
-		connectionVC.view.translatesAutoresizingMaskIntoConstraints = false
+    func addConnectionViewController() {
+        guard connectionViewController == nil else {
+            return
+        }
+
+        let window = (UIApplication.shared.delegate!.window!)!
+
+        let connectionVC = UIStoryboard(name: "ConnectionView", bundle: nil).instantiateInitialViewController() as! MatrixConnectionViewController
+        connectionVC.view.translatesAutoresizingMaskIntoConstraints = false
         self.addChild(connectionVC)
-		window.addSubview(connectionVC.view)
+        window.addSubview(connectionVC.view)
 
-		NSLayoutConstraint.activate([
+        NSLayoutConstraint.activate([
             connectionVC.view.rightAnchor.constraint(equalTo: window.safeAreaLayoutGuide.rightAnchor, constant: -8.0),
             connectionVC.view.topAnchor.constraint(equalTo: window.safeAreaLayoutGuide.topAnchor, constant: 8.0),
             connectionVC.view.leftAnchor.constraint(greaterThanOrEqualTo: window.safeAreaLayoutGuide.leftAnchor, constant: 0.0),
             connectionVC.view.bottomAnchor.constraint(lessThanOrEqualTo: window.safeAreaLayoutGuide.bottomAnchor, constant: 0.0)
-			])
+            ])
 
         connectionVC.didMove(toParent: self)
 
-		connectionViewController = connectionVC
-	}
+        connectionViewController = connectionVC
+    }
 }
