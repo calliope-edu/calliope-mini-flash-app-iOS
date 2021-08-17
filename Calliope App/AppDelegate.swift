208be18709d37bbb16456309cdf9d7c82d1d41c3
jasperp92 <jasperprecht@Calliopes-Air.local>
2021-08-17 18:06:23 +0200

    Revert "added news url and reset button in settings"
    This reverts commit 389109ac7262d849fa43c51489e5645786159cca.

--- Calliope App/AppDelegate.swift
+++ Calliope App/AppDelegate.swift
@@ -11,18 +11,17 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
-    var window: UIWindow?
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        Settings.registerDefaults()
-        Settings.resetIfNecessary()
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		Settings.registerDefaults()
         Settings.updateAppVersion()
         Styles.setupGlobalFont()
-        
+
         UNUserNotificationCenter.current().delegate = self
         
-        return true
-    }
+		return true
+	}
 
 	func applicationWillResignActive(_ application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
