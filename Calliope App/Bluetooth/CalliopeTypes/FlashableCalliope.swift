7ebe3129733126e86b0b0546d4c948d39276d2ee
Lukas Januschke, itestra GmbH <januschke@itestra.de>
2023-11-15 18:17:12 +0100

    temporarily disable partial flashing for v1 and v2

--- Calliope App/Bluetooth/CalliopeTypes/FlashableCalliope.swift
+++ Calliope App/Bluetooth/CalliopeTypes/FlashableCalliope.swift
@@ -380,6 +380,17 @@ class CalliopeV1AndV2: FlashableCalliope {
         
         try triggerDfuMode()
     }
+    
+    internal override func startPartialFlashing() {
+        // TODO: Solve Partial Flashing Errors
+        // Partial Flashing does not work entirely functional with the current Version of the Firmware. We therefor fallback to Full Flashing until this has been solved.
+        // Partial Flashing starts, but the Calliope disconnects unexpectedly after around 6% have been transfered.
+        do {
+            try startFullFlashing()
+        } catch {
+            LogNotify.log("Tried reverting to Full Flashing, but failed")
+        }
+    }
 }
 
 //MARK: Calliope V3
