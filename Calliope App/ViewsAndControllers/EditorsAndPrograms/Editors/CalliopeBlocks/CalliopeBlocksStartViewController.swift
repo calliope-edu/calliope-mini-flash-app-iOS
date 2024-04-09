9ed69bf448bf4585ba6993b56494dcd49ffdf6c3
Lukas Januschke, itestra GmbH <januschke@itestra.de>
2024-04-09 16:40:28 +0200

    add links and edits to calliope mini blocks editor page

--- Calliope App/ViewsAndControllers/EditorsAndPrograms/Editors/CalliopeBlocks/CalliopeBlocksStartViewController.swift
+++ Calliope App/ViewsAndControllers/EditorsAndPrograms/Editors/CalliopeBlocks/CalliopeBlocksStartViewController.swift
@@ -11,9 +11,17 @@ import UIKit
 class CalliopeBlocksStartViewController: UIViewController {
 
     @IBOutlet weak var mainStackView: UIStackView!
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // create tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped(gesture:)))
+
+        // add it to the image view;
+        imageView.addGestureRecognizer(tapGesture)
+        // make sure imageView can be interacted with by user
+        imageView.isUserInteractionEnabled = true
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -44,5 +52,17 @@ class CalliopeBlocksStartViewController: UIViewController {
         let landscape: Bool = parentSize.width > parentSize.height
         return CGSize(width: landscape ? parentSize.width / 2.0 : parentSize.width, height: landscape ? parentSize.height : parentSize.height / 2.0)
     }
-
+    
+    @objc func imageTapped(gesture: UIGestureRecognizer) {
+        // if the tapped view is a UIImageView then set it to imageview
+        if let url = URL(string: "https://apps.apple.com/de/app/scrub-web-browser/id1569777095") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    @IBAction func showGetStartedSite(_ sender: Any) {
+        if let url = URL(string: "https://calliope.cc") {
+            UIApplication.shared.open(url)
+        }
+    }
 }
