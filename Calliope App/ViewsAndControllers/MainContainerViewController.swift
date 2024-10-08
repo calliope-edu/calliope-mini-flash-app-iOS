7178e44852d940dacd581f2200bdbdd40932636b
Serge Kotchourko, itestra GmbH <kotchourko@itestra.de>
2024-10-08 18:56:51 +0200

    fix(ui): #48 preliminary fix for iOS18 TabBar changes (possibly unstable)

--- Calliope App/ViewsAndControllers/MainContainerViewController.swift
+++ Calliope App/ViewsAndControllers/MainContainerViewController.swift
@@ -10,18 +10,30 @@ import SnapKit
 
 class MainContainerViewController: UITabBarController, UITabBarControllerDelegate {
 
-	@IBOutlet weak var matrixConnectionView: UIView!
+    @IBOutlet weak var matrixConnectionView: UIView!
 
-	weak var connectionViewController: MatrixConnectionViewController!
+    weak var connectionViewController: MatrixConnectionViewController!
 
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        updateTraitOverrides()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
         DispatchQueue.main.async {
             //called on the non-concurrent main queue so no further synchronization necessary
             self.addConnectionViewController()
         }
-	}
+    }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+
+        updateTraitOverrides()
+    }
 
     func addConnectionViewController() {
         guard connectionViewController == nil else {
@@ -29,7 +41,7 @@ class MainContainerViewController: UITabBarController, UITabBarControllerDelegat
         }
 
         let window = (UIApplication.shared.delegate!.window!)!
-        
+
         DispatchQueue.main.async {
             let connectionVC = UIStoryboard(name: "ConnectionView", bundle: nil).instantiateInitialViewController() as! MatrixConnectionViewController
             connectionVC.view.translatesAutoresizingMaskIntoConstraints = false
@@ -37,14 +49,24 @@ class MainContainerViewController: UITabBarController, UITabBarControllerDelegat
             window.addSubview(connectionVC.view)
 
             NSLayoutConstraint.activate([
-                connectionVC.view.rightAnchor.constraint(equalTo: window.safeAreaLayoutGuide.rightAnchor, constant: -8.0),
-                connectionVC.view.topAnchor.constraint(equalTo: window.safeAreaLayoutGuide.topAnchor, constant: 8.0),
-                connectionVC.view.leftAnchor.constraint(greaterThanOrEqualTo: window.safeAreaLayoutGuide.leftAnchor, constant: 0.0),
-                connectionVC.view.bottomAnchor.constraint(lessThanOrEqualTo: window.safeAreaLayoutGuide.bottomAnchor, constant: 0.0)
-                ])
+                                            connectionVC.view.rightAnchor.constraint(equalTo: window.safeAreaLayoutGuide.rightAnchor, constant: -8.0),
+                                            connectionVC.view.topAnchor.constraint(equalTo: window.safeAreaLayoutGuide.topAnchor, constant: 8.0),
+                                            connectionVC.view.leftAnchor.constraint(greaterThanOrEqualTo: window.safeAreaLayoutGuide.leftAnchor, constant: 0.0),
+                                            connectionVC.view.bottomAnchor.constraint(lessThanOrEqualTo: window.safeAreaLayoutGuide.bottomAnchor, constant: 0.0)
+                                        ])
 
             connectionVC.didMove(toParent: self)
             self.connectionViewController = connectionVC
         }
+    }
+
+    private func updateTraitOverrides() {
+        guard #available(iOS 18.0, *)
+        else {
+            return
+        }
+
+        // Update the current size class to display original design
+        traitOverrides.horizontalSizeClass = .compact
     }
 }
