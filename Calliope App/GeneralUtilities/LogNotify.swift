9ba1e25a6e972f2c5cc241a289d619602224ff02
Serge Kotchourko, itestra GmbH <kotchourko@itestra.de>
2024-11-05 15:02:35 +0100

    refactor(logging): refactor logging to allow for levels and increased readability

--- Calliope App/GeneralUtilities/LogNotify.swift
+++ Calliope App/GeneralUtilities/LogNotify.swift
@@ -2,17 +2,20 @@ import Foundation
 
 class LogNotify {
 
-	static let debug = true
-    
+    enum LEVEL: String {
+        case INFO, DEBUG, ERROR
+    }
+
+
     static let logNotifyName = Notification.Name("cc.calliope.mini.logger")
 
-    public class func log(_ msg: @autoclosure () -> String, fileName: String = #file, lineNumber: Int = #line) {
-		if debug {
-			let lastPathComponent = (fileName as NSString).lastPathComponent
-			let filenameOnly = lastPathComponent.components(separatedBy: ".")[0]
-			let extendedMessage = "\(filenameOnly):\(lineNumber) \(msg())"
-			NSLog(extendedMessage)
-		}
+    public class func log(_ msg: @autoclosure () -> String, level: LEVEL = .INFO, fileName: String = #file, lineNumber: Int = #line) {
+        #if DEBUG
+        let lastPathComponent = (fileName as NSString).lastPathComponent
+        let filenameOnly = lastPathComponent.components(separatedBy: ".")[0]
+        let extendedMessage = "[\(level.rawValue.padding(toLength: 5, withPad: " ", startingAt: 0))] [\(filenameOnly):\(lineNumber)] \(msg())"
+        NSLog(extendedMessage)
+        #endif
     }
-    
+
 }
