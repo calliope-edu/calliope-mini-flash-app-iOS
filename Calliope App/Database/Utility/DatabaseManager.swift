a211ca93373aeba6b96429ee1caad2e94e28ec8f
Serge Kotchourko, itestra GmbH <kotchourko@itestra.de>
2024-11-12 17:20:58 +0100

    fix(db): #61 migration to correct types for keys

--- Calliope App/Database/Utility/DatabaseManager.swift
+++ Calliope App/Database/Utility/DatabaseManager.swift
@@ -30,17 +30,72 @@ class DatabaseManager {
                 return
             }
 
-            var migrator = DatabaseMigrator()
+            try migration(databaseQueue);
 
-            #if DEBUG
-            migrator.eraseDatabaseOnSchemaChange = true
-            #endif
-
-            try createTables(databaseQueue)
-            try migrator.migrate(databaseQueue)
         } catch {
             LogNotify.log("Database setup failed: \(error)")
         }
+    }
+
+
+    func migration(_ databaseQueue: DatabaseQueue) throws {
+        var migrator = DatabaseMigrator()
+        #if DEBUG
+        // migrator.eraseDatabaseOnSchemaChange = true
+        #endif
+
+        // initial version of sensor data view
+        try createTables(databaseQueue)
+
+        // migration of key to correct types
+        migrator.registerMigration("Change FK column types to integer") { db in
+            try db.create(table: "charts_migration") { t in
+                t.autoIncrementedPrimaryKey("id")
+                t.column("sensorType", .any)
+                t.column("projectsId", .integer).notNull()
+                t.foreignKey(["projectsId"], references: "projects", onDelete: .cascade)
+            }
+
+            let chartRows = try Row.fetchCursor(db, sql: "SELECT * FROM charts")
+            while let row = try chartRows.next() {
+                try db.execute(
+                    sql: "INSERT INTO charts_migration (id, sensorType, projects) VALUES (?, ?, ?)",
+                    arguments: [
+                      row["id"],
+                      row["sensorType"],
+                      row["projectsId"] as Int64
+                      ])
+            }
+
+            try db.drop(table: "charts")
+            try db.rename(table: "charts_migration", to: "charts")
+
+        try db.create(table: "value_migration") { t in
+            t.autoIncrementedPrimaryKey("id")
+            t.column("value", .text).notNull()
+            t.column("time", .text).notNull()
+            t.column("chartsId", .integer).notNull()
+            t.foreignKey(["chartsId"], references: "charts", onDelete: .cascade)
+        }
+
+            let valueRows = try Row.fetchCursor(db, sql: "SELECT * FROM value")
+            while let row = try valueRows.next() {
+                try db.execute(
+                    sql: "INSERT INTO value_migration (id, value, time, chartsId) VALUES (?, ?, ?, ?)",
+                    arguments: [
+                      row["id"],
+                      row["value"],
+                      row["time"],
+                      row["chartsId"] as Int64
+                      ])
+            }
+
+            try db.drop(table: "value")
+            try db.rename(table: "value_migration", to: "value")
+        }
+
+
+        try migrator.migrate(databaseQueue)
     }
 
     func createTables(_ dbQueue: DatabaseQueue) throws {
@@ -67,7 +122,6 @@ class DatabaseManager {
                 try Value.createTable(in: db)
             }
         }
-
     }
 
     static func notifyChange() {
