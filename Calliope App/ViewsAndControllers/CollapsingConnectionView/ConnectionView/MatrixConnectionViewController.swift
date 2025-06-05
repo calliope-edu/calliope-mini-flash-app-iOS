1413614701097a68b5aaa2df642959a4c38b0f89
Serge Kotchourko, itestra GmbH <kotchourko@itestra.de>
2025-06-05 14:56:25 +0200

    fix(ble): correct state determination of BLE device

--- Calliope App/ViewsAndControllers/CollapsingConnectionView/ConnectionView/MatrixConnectionViewController.swift
+++ Calliope App/ViewsAndControllers/CollapsingConnectionView/ConnectionView/MatrixConnectionViewController.swift
@@ -349,7 +349,7 @@ class MatrixConnectionViewController: UIViewController, CollapsingViewController
         if let usageReadyCalliope = calliope.usageReadyCalliope, usageReadyCalliope.rebootingIntoDFUMode, calliope.state == .discovered {
             self.collapseButton.connectionState = .connected
         } else if calliope.state == .wrongMode || calliope.state == .discovered {
-            self.collapseButton.connectionState = (attemptReconnect || reconnecting) ? .disconnected : .connected
+            self.collapseButton.connectionState = (attemptReconnect || reconnecting) ? .connected : .disconnected
         } else if calliope.state == .usageReady {
             self.collapseButton.connectionState = .connected
             LogNotify.log("last pattern:\r\(matrixView.getMatrixString())")
