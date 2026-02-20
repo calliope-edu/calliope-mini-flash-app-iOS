f91813b03690d13c14b19c4b0543d6feec4efc29
Tobias Aichele, itestra GmbH <aichele@itestra.de>
2026-02-20 15:24:16 +0100

    Added a swiftui widget to the lofi apps page.

--- Calliope App/ViewsAndControllers/LofiApps/LofiAppsViewController.swift
+++ Calliope App/ViewsAndControllers/LofiApps/LofiAppsViewController.swift
@@ -6,51 +6,16 @@
 //
 
 import UIKit
+import SwiftUI
 
 /// A blank view controller displayed under the new "LofiApps" tab.
 final class LofiAppsViewController: UIViewController {
-    @IBOutlet private weak var collectionView: UICollectionView!
-    private var apps: [String] = ["Test 1", "Test 2", "Test 3", "Test 1", "Test 2", "Test 3"]
 
+    @IBSegueAction func addSwiftUIView(_ coder: NSCoder) -> UIViewController? {
+        return UIHostingController(coder: coder, rootView: SwiftUIView())
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureCollectionView()
-        collectionView.reloadData()
-    }
-
-    private func configureCollectionView() {
-        // Register the nib if you used a separate xib for the cell
-        // collectionView.register(UINib(nibName: "PhotoCell", bundle: nil),
-        //                         forCellWithReuseIdentifier: PhotoCell.reuseIdentifier)
-
-        // Set datasource & delegate (if you use the classic API)
-        collectionView.dataSource = self
-        // collectionView.delegate   = self
-    }
-}
-
-extension LofiAppsViewController: UICollectionViewDataSource {
-
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1               // you can have more than one section
-    }
-
-    func collectionView(_ collectionView: UICollectionView,
-                        numberOfItemsInSection section: Int) -> Int {
-        return apps.count
-    }
-
-    func collectionView(_ collectionView: UICollectionView,
-                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        // Dequeue the custom cell
-        guard let cell = collectionView.dequeueReusableCell(
-                withReuseIdentifier: LofiAppsCollectionViewCell.reuseIdentifier,
-                for: indexPath) as? LofiAppsCollectionViewCell else {
-            fatalError("Unable to dequeue PhotoCell")
-        }
-
-        let app = apps[indexPath.item]
-        cell.configure(app)
-        return cell
     }
 }
---
+++ Calliope App/ViewsAndControllers/LofiApps/SwiftUIView.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUIView.swift
+//  Calliope App
+//
+//  Created by Calliope on 20.02.26.
+//  Copyright © 2026 calliope. All rights reserved.
+//
+
+import Foundation
+
+import SwiftUI
+struct SwiftUIView : View {
+    var body: some View {
+        Text("This is a swiftUI view 👋")
+    }
+}
