1bebeaebd26940a9ba6d465ec547f077d8660616
Tobias Aichele, itestra GmbH <aichele@itestra.de>
2026-03-13 14:59:08 +0100

    Reduced bluetooth read/write timeouts to reduce freezing on calliope disconnect.

--- Calliope App/Bluetooth/BluetoothConstants.swift
+++ Calliope App/Bluetooth/BluetoothConstants.swift
@@ -11,8 +11,8 @@ struct BluetoothConstants {
 	static let discoveryTimeout = 20.0
 	static let connectTimeout = 5
 	static let serviceDiscoveryTimeout = 10.0
-	static let readTimeout = 10.0
-	static let writeTimeout = 10.0
+	static let readTimeout = 1.0
+	static let writeTimeout = 1.0
 	static let startDfuProcessDelay = 2.0
 
 	static let maxRetryCount = 5;
