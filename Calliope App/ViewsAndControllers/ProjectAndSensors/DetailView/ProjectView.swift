a520034e2b35a37280da3e7efedb6c85d8e05bdd
Tobias Aichele, itestra GmbH <aichele@itestra.de>
2026-04-17 14:42:12 +0200

    Replaced the ProjectView with a SwiftUI View.

---
+++ Calliope App/ViewsAndControllers/ProjectAndSensors/DetailView/ProjectView.swift
@@ -0,0 +1,16 @@
+//
+//  ProjectView.swift
+//  Calliope App
+//
+//  Created by Calliope on 17.04.26.
+//  Copyright © 2026 calliope. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ProjectView: View {
+    var body: some View {
+        Text("This is a swiftUI view 👋")
+    }
+}
