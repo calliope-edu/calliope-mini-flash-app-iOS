b1970c9d046de9a1969f6fbd86e0a565d4ae717e
Tobias Aichele, itestra GmbH <aichele@itestra.de>
2026-06-03 10:33:27 +0200

    feature: WebView got functionality to load html

--- Calliope App/ViewsAndControllers/Common/WebView.swift
+++ Calliope App/ViewsAndControllers/Common/WebView.swift
@@ -11,16 +11,19 @@ import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
-    let url: URL
+    @State var url: URL?
+    @State var html: String?
 
     func makeUIView(context: Context) -> WKWebView {
-
         return WKWebView()
     }
-    
-    func updateUIView(_ webView: WKWebView, context: Context) {
 
-        let request = URLRequest(url: url)
-        webView.load(request)
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        if url != nil {
+            let request = URLRequest(url: url!)
+            webView.load(request)
+        } else if html != nil {
+            webView.loadHTMLString(html!, baseURL: nil)
+        }
     }
 }
