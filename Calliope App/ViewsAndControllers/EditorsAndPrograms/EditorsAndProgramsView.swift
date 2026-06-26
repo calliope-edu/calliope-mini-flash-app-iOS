5c6f94a1a4fde27af25780fb253957bba7e4acf3
Tobias Aichele, itestra GmbH <aichele@itestra.de>
2026-06-26 18:35:11 +0200

    feature: draft the ui of the editors and projects page

--- Calliope App/ViewsAndControllers/EditorsAndPrograms/EditorsAndProgramsView.swift
+++ Calliope App/ViewsAndControllers/EditorsAndPrograms/EditorsAndProgramsView.swift
@@ -11,26 +11,41 @@ import SwiftUI
 
 struct EditorsAndProgramsView: View {
     @ObservedObject var viewModel: EditorsAndProgramsViewModel
-    
+
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 500))]) {
-                editorsTile.tiled(color: Color.calliopeLightgray, takeRemainingSpace: true, padding: 30)
-                originalProgramCalliope3Tile.tiled(color: Color.calliopeTurqoise, takeRemainingSpace: true, padding: 30)
-                originalProgramCalliope12Tile.tiled(color: Color.calliopePink, takeRemainingSpace: true, padding: 30)
-                makeCodeQRCodeTile.tiled(color: Color.calliopeLightgray, takeRemainingSpace: true, padding: 30)
-                fileToCalliopeTile.tiled(color: Color.calliopeLightgray, takeRemainingSpace: true, padding: 30)
-                programsTile.tiled(color: Color.calliopeLightgray, takeRemainingSpace: true, padding: 30)
-            }.frame(maxWidth: .infinity)
-        }.padding()
+
+        GeometryReader { geo in
+            ScrollView {
+                if geo.size.width > 1200 {
+                    TwoColumnLayout {
+                        items
+                    }
+                } else {
+                    OneColumnLayout {
+                        items
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+
+    @ViewBuilder
+    var items: some View {
+        editorsTile.tiled(color: Color.calliopeLightgray, takeRemainingSpace: true, padding: 30)
+        programsTile.tiled(color: Color.calliopeLightgray, takeRemainingSpace: true, padding: 30)
+        makeCodeQRCodeTile.tiled(color: Color.calliopeLightgray, takeRemainingSpace: true, padding: 30)
+        fileToCalliopeTile.tiled(color: Color.calliopeLightgray, takeRemainingSpace: true, padding: 30)
+        originalProgramCalliope3Tile.tiled(color: Color.calliopeTurqoise, takeRemainingSpace: true, padding: 30)
+        originalProgramCalliope12Tile.tiled(color: Color.calliopePink, takeRemainingSpace: true, padding: 30)
     }
 
     var editorsTile: some View {
         VStack(alignment: .leading) {
             Text("You can program your Calliope mini with the help of the editors.").fontWeight(.bold)
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 250))]) {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 250))], spacing: 40) {
                 ForEach(0..<viewModel.editors.count) { i in
-                    EditorTile(name: viewModel.editors[i])
+                    EditorTile(config: viewModel.editors[i])
                 }
             }
         }
@@ -39,62 +54,130 @@ struct EditorsAndProgramsView: View {
     var originalProgramCalliope3Tile: some View {
         VStack(alignment: .leading) {
             Text("Calliope mini 3: Download the original program of the Calliope mini 3. Connect and transfer the program!").fontWeight(.bold)
-            boxButton(label: "Start program", action: { print("Start Calliop mini 3 program")})
+                .foregroundStyle(Color.white)
+            HStack {
+                Spacer()
+                Image("startprogramm3").resizable().scaledToFit().frame(maxWidth: 300)
+                Spacer()
+            }.padding(.vertical, 16)
+            boxButton(label: "Start program", iconName: "button_icon_upload", action: { print("Start Calliop mini 3 program") })
         }
     }
 
     var originalProgramCalliope12Tile: some View {
         VStack(alignment: .leading) {
-            Text("Calliope mini 1+2: Download the original program of the Calliope mini 1 and Calliope mini 2. Connect and transfer the program!").fontWeight(.bold)
-            boxButton(label: "Start program", action: { print("Start Calliop mini 1+2 program")})
+            Text("Calliope mini 1+2: Download the original program of the Calliope mini 1 and Calliope mini 2. Connect and transfer the program!")
+                .fontWeight(.bold).foregroundStyle(Color.white)
+            HStack {
+                Spacer()
+                Image("startprogramm1+2").resizable().scaledToFit().frame(maxWidth: 300)
+                Spacer()
+            }.padding(.vertical, 16)
+            boxButton(label: "Start program", iconName: "button_icon_upload", action: { print("Start Calliop mini 1+2 program") })
         }
     }
 
     var makeCodeQRCodeTile: some View {
         VStack(alignment: .leading) {
             Text("Open a program in MakeCode using a QR code. Simply scan it and off you go!").fontWeight(.bold)
-            boxButton(label: "Scan", action: { print("Scan QR Code")})
+            boxButton(label: "Scan", iconName: "qr_code_scan_button", action: { print("Scan QR Code") })
         }
     }
 
     var fileToCalliopeTile: some View {
         VStack(alignment: .leading) {
-            Text("Transfer program from your iPhone or iPad directly to your Calliope mini or save them in your Calliope mini folder.").fontWeight(.bold)
-            boxButton(label: "Choose File", action: { print("Choose file")})
+            Text("Transfer program from your iPhone or iPad directly to your Calliope mini or save them in your Calliope mini folder.").fontWeight(
+                .bold
+            )
+            boxButton(label: "Choose File", iconName: "doc", isSystemImage: true, action: { print("Choose file") })
         }
     }
 
     var programsTile: some View {
         VStack(alignment: .leading) {
-            Text("A long press takes you to the option of transfering, sharing and deleting your programs stored in the Calliope mini folder.").fontWeight(.bold)
+            Text("A long press takes you to the option of transfering, sharing and deleting your programs stored in the Calliope mini folder.")
+                .fontWeight(.bold)
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 250))], spacing: 40) {
+                ForEach(0..<viewModel.programs.count) { i in
+                    ProgramTile(config: viewModel.programs[i]).tiled(color: Color.calliopeGray)
+                }
+            }
         }
     }
-    
-    func boxButton(label: String, action: @escaping () -> Void) -> some View {
+
+    func boxButton(label: String, iconName: String, isSystemImage: Bool = false, action: @escaping () -> Void) -> some View {
         return Button {
             action()
         } label: {
-            Text(label)
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.calliopeGreen)
-                .foregroundColor(.white)
-                .cornerRadius(12)
+            HStack {
+                Text(label)
+                Spacer()
+                (isSystemImage ? Image(systemName: iconName) : Image(iconName))
+                    .resizable().scaledToFit().frame(width: 30, height: 30)
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.calliopeGreen)
+            .foregroundColor(.white)
+            .cornerRadius(12)
+
         }
     }
 }
 
-struct EditorTile: View {
-    let name: String
-    
+struct OneColumnLayout<Content: View>: View {
+    @ViewBuilder let content: Content
+
     var body: some View {
-        Text(name)
+        MasonryLayout(columns: 1) {
+            content
+        }
+    }
+}
+
+struct TwoColumnLayout<Content: View>: View {
+    @ViewBuilder let content: Content
+
+    var body: some View {
+        MasonryLayout(columns: 2) {
+            content
+        }
+    }
+}
+
+struct ProgramTile: View {
+    let config: ProgramTileConfig
+
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(config.name).foregroundStyle(Color.white)
+                Text(config.lastUsed.formatted()).foregroundStyle(Color.white)
+            }
+            Spacer()
+            Image("button_icon_upload").resizable().scaledToFit().frame(maxWidth: 30)
+        }.frame(maxWidth: 250)
+    }
+}
+
+struct EditorTile: View {
+    let config: EditorTileConfig
+
+    var body: some View {
+        VStack {
+            Image(config.iconName)
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 175)
+            Text(config.name)
+        }
     }
 }
 
 struct EditorAndProgramsView_Previews: PreviewProvider {
     static var previews: some View {
         let viewModel = EditorsAndProgramsViewModel()
-        EditorsAndProgramsView(viewModel: viewModel)
+        EditorsAndProgramsView(viewModel: viewModel).previewInterfaceOrientation(.landscapeLeft)
+        EditorsAndProgramsView(viewModel: viewModel).previewInterfaceOrientation(.portrait)
     }
 }
--- Calliope App/ViewsAndControllers/EditorsAndPrograms/EditorsAndProgramsViewModel.swift
+++ Calliope App/ViewsAndControllers/EditorsAndPrograms/EditorsAndProgramsViewModel.swift
@@ -8,6 +8,27 @@
 
 import Foundation
 
+struct EditorTileConfig {
+    let name: String
+    let iconName: String
+}
+
+struct ProgramTileConfig {
+    let name: String
+    let lastUsed: Date
+}
+
 class EditorsAndProgramsViewModel: ObservableObject {
-    @Published var editors: [String] = ["Makecode", "Open Roberta Lab", "Calliope mini Blocks", "MicroPython", "Arcade (USB only)"]
+    @Published var editors: [EditorTileConfig] = [
+        EditorTileConfig(name: "Makecode", iconName: "editors_makecode"),
+        EditorTileConfig(name: "Open Roberta Lab", iconName: "editors_nepo"),
+        EditorTileConfig(name: "Calliope mini Blocks", iconName: "editors_blocks_transparent"),
+        EditorTileConfig(name: "Micropython", iconName: "editors_python"),
+        EditorTileConfig(name: "Arcade (USB only)", iconName: "editors_swift")
+    ]
+    
+    @Published var programs: [ProgramTileConfig] = [
+        ProgramTileConfig(name: "Test 1", lastUsed: Date.now),
+        ProgramTileConfig(name: "Test 2", lastUsed: Date.now.addingTimeInterval(100)),
+    ]
 }
